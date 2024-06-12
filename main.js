beda7b53988064685c873c80466d9f233a70fd0e
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-06-12 20:36:54 +0200

    Feat: add prompt to choose between odd and even numbers

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione)
 // creare un prompt e chiedere l'user di scegliere pari o dispari
+const pariDispari = prompt ("Pari o dispari?");
+console.log(pariDispari);
 // creare un prompt numerico per l'User facendogli inserire un numero da 1 a 5
+// faccio un controlla che il numero inserito sia da 1 a 5
 // generiamo un numero da 1 a 5 per il computer
 // faccio la somma dei due numeri
 // SE il mio numero è quanto scelo dal user...
