1defeb88f18e8781448852c5dce13095f179994a
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-06-12 20:02:50 +0200

    Build: add logical diagram of JS steps

---
+++ main.js
@@ -0,0 +1,9 @@
+// Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione)
+// creare un prompt e chiedere l'user di scegliere pari o dispari
+// creare un prompt numerico per l'User facendogli inserire un numero da 1 a 5
+// generiamo un numero da 1 a 5 per il computer
+// faccio la somma dei due numeri
+// SE il mio numero è quanto scelo dal user...
+// stampo "Sei il vincitore"
+// SE diverso da quanto sceltro dall'user...
+// stampa "Hai perso! il computer ti ha battutto"
