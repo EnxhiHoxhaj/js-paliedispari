65b94fb394a79ba60f7d6b4ce0b0ef86c0200550
EnxhiHoxhaj <hoxhajenxhi1@gmail.com>
2024-06-12 21:09:46 +0200

    Feat: add check whether odd or even was chosen

--- main.js
+++ main.js
@@ -1,7 +1,15 @@
 // Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione)
 // creare un prompt e chiedere l'user di scegliere pari o dispari
 const pariDispari = prompt ("Pari o dispari?");
-console.log(pariDispari);
+// Controllo che il valore inserito sia pari o dispari
+const sceltoPD = pariDispari;
+let ritenta;
+if (sceltoPD === "pari" || sceltoPD === "dispari") {
+    console.log(sceltoPD);
+}else{
+    ritenta = ("Puoi scegliere solo tra pari e dispari! refresha la pagina per ritentare");
+    console.log(ritenta);
+}
 // creare un prompt numerico per l'User facendogli inserire un numero da 1 a 5
 const userNum = parseInt(prompt("Segli un numero da 1 a 5"));
 // faccio un controlla che il numero inserito sia da 1 a 5
@@ -9,7 +17,7 @@ if(userNum >= 1 && userNum <= 5){
     let numeroScelto = userNum;
     console.log(numeroScelto);
 } else {
-    let ritenta = ("Se non scegli un numero da 1 a 5 non puoi giocare! refresha la pagina per ritentare");
+    ritenta = ("Se non scegli un numero da 1 a 5 non puoi giocare! refresha la pagina per ritentare");
     console.log(ritenta);
 }
 // generiamo un numero da 1 a 5 per il computer
